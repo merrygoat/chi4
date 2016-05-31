737ec6a369c6b99abfff5428ac1b4fc06ab78f33
Peter Crowther <pc9836@my.bristol.ac.uk>
2016-05-31 14:25:38 +0100

    Started addition of 2D stack processing.

--- chi4.py
+++ chi4.py
@@ -27,8 +27,12 @@ def cdistmethod(data, numframes, averageparticles, dimensions, numberdensity, th
     chisquared = distancesquared[:, 2] - distance[:, 3]
 
     # normalise chi squared
+<<<<<<< Updated upstream
     chisquared = chisquared/(numberdensity*averageparticles)
 
+=======
+    chisquared = chisquared*numberdensity*averageparticles
+>>>>>>> Stashed changes
     return chisquared
 
 def xyztocg(filename):
@@ -61,30 +65,59 @@ def xyztocg(filename):
 
 def main():
 
+<<<<<<< Updated upstream
     if len(argv) != 5:
         print("Incorrect syntax. Use Chi4.py filename.txt num_spatial_dimensions chi4_threshold number_density_in_sigma")
+=======
+    t = time()
+
+
+    if len(argv) != 5:
+        print("Incorrect syntax. Use Chi4.py filename.txt num_spatial_dimensions chi4_threshold number_density")
+>>>>>>> Stashed changes
         exit()
     filename = argv[1]
     dimensions = int(argv[2])
     threshold = float(argv[3])
     numberdensity = float(argv[4])
+<<<<<<< Updated upstream
+=======
+    zslices = 1
 
-    if filename.endswith("xyz"):
-        xyztocg(filename)
-        filename += ".cg"
+    if dimensions == 2:
+        zslices = 1
 
-    data = np.loadtxt(filename)
+    chisquaredresults = [[] for x in range(zslices)]
 
-    numcolumns = data.shape[1]  # how many columns are there in the inut data?
-    numframes = int(max(data[:, numcolumns - 2]) + 1)  # assume framenumber is penultimate column
-    averageparticles = data.shape[0]/float(numframes)
-    sortedmatrix = []
+    for i in range(0, zslices):
+        if zslices > 1:
+            filename = "slice_" + i + "_ " + filename
 
-    for i in xrange(0, numframes):
-        sortedmatrix.append(data[data[:, numcolumns - 2] == i, :])
+        if filename.endswith("xyz"):
+            xyztocg(filename)
+            filename += ".cg"
 
+        data = np.loadtxt(filename)
+>>>>>>> Stashed changes
+
+        numcolumns = data.shape[1]  # how many columns are there in the inut data?
+        numframes = int(max(data[:, numcolumns - 2]) + 1)  # assume framenumber is penultimate column
+        averageparticles = data.shape[0]/float(numframes)
+        sortedmatrix = []
+
+        for j in xrange(0, numframes):
+            sortedmatrix.append(data[data[:, numcolumns - 2] == j, :])
+
+        chisquaredresults[i] = cdistmethod(sortedmatrix, numframes, averageparticles, dimensions, numberdensity, threshold)
+
+        np.savetxt(filename + "_chi4.txt", )
+
+<<<<<<< Updated upstream
     a = time()
     np.savetxt(filename + "_chi4.txt", cdistmethod(sortedmatrix, numframes, averageparticles, dimensions, numberdensity, threshold))
     print time()-a
+=======
+    print time()-t
+>>>>>>> Stashed changes
 
 main()
