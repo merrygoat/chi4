5f7abea19d632808ee00d109cedb6483a8a716b1
Peter <pc9836@bristol.ac.uk>
2017-04-11 22:46:45 +0100

    Updated to Python 3 and some other modifications to work with Pastore 2D trajectories

--- chi4.py
+++ chi4.py
@@ -8,30 +8,30 @@ def cdistmethod(data, numframes, averageparticles, dimensions, numberdensity, th
     distance = np.zeros((numframes, 4))
     distancesquared = np.zeros((numframes, 3))
 
-    for refframenumber in xrange(0, numframes):
-        for curframenumber in xrange(refframenumber+1, numframes):
+    for refframenumber in range(0, numframes):
+        for curframenumber in range(refframenumber + 1, numframes):
             sqrtprod = cdist(data[refframenumber][:, 0:dimensions], data[curframenumber][:, 0:dimensions])
             numobservations = data[refframenumber][:, 0:dimensions].shape[0] * data[curframenumber][:, 0:dimensions].shape[0]
 
-            result = float((np.sum(sqrtprod < threshold)))/np.sqrt(numobservations)
-            distance[curframenumber-refframenumber, 0:2] += [result, 1]
-            distancesquared[curframenumber-refframenumber, 0:2] += [(result*result), 1]
+            result = float((np.sum(sqrtprod < threshold))) / np.sqrt(numobservations)
+            distance[curframenumber - refframenumber, 0:2] += [result, 1]
+            distancesquared[curframenumber - refframenumber, 0:2] += [(result * result), 1]
 
     # normalise by dividing by number of observations
     with np.errstate(divide='ignore', invalid='ignore'):  # suppress divide by zero warning since value is 0 at T=0
-        distance[:, 2] = distance[:, 0]/distance[:, 1]
-        distancesquared[:, 2] = distancesquared[:, 0]/distancesquared[:, 1]
+        distance[:, 2] = distance[:, 0] / distance[:, 1]
+        distancesquared[:, 2] = distancesquared[:, 0] / distancesquared[:, 1]
     # square distance
-    distance[:, 3] = distance[:, 2]**2
+    distance[:, 3] = distance[:, 2] ** 2
 
     chisquared = distancesquared[:, 2] - distance[:, 3]
 
     # normalise chi squared
-    chisquared = chisquared*numberdensity*averageparticles
+    chisquared = chisquared * numberdensity * averageparticles
     return chisquared
 
-def xyztocg(filename, simulationdata = 1):
 
+def xyztocg(filename, simulationdata=1):
     inputfile = open(filename, 'r')
 
     outputfile = open(filename + ".cg", 'w')
@@ -44,14 +44,10 @@ def xyztocg(filename, simulationdata = 1):
         numparticles = int(line)  # read number of particles from first line of frame
         line = inputfile.readline()  # read comment from second line of frame
 
-        for i in xrange(0, numparticles):
+        for i in range(0, numparticles):
             line = inputfile.readline()
             outputchunk = line.split()
-            outputfile.write(outputchunk[1] + "\t" +
-                             outputchunk[2] + "\t" +
-                             outputchunk[3].rstrip("\n") + "\t" +
-                             str(framenumber) + "\t" +
-                             str(particlenumber) + "\n")
+            outputfile.write(outputchunk[1] + "\t" + outputchunk[2] + "\t" + outputchunk[3].rstrip("\n") + "\t" + str(framenumber) + "\t" + str(particlenumber) + "\n")
             particlenumber += 1
         framenumber += 1
         if simulationdata == 1:
@@ -61,9 +57,7 @@ def xyztocg(filename, simulationdata = 1):
 
 
 def main():
-
     t = time()
-
 
     if len(argv) != 5:
         print("Incorrect syntax. Use Chi4.py filename.txt num_spatial_dimensions chi4_threshold number_density")
@@ -72,33 +66,27 @@ def main():
     dimensions = int(argv[2])
     threshold = float(argv[3])
     numberdensity = float(argv[4])
-    zslices = 1
-    chisquaredresults = [[] for x in range (zslices)]
 
-    for i in range(0, zslices):
-        if zslices > 1:
-            filename = "slice_" + i + "_ " + filename
+    if filename.endswith("xyz"):
+        xyztocg(filename)
+        filename += ".cg"
 
-        if filename.endswith("xyz"):
-            xyztocg(filename)
-            filename += ".cg"
-    
-        data = np.loadtxt(filename)
-    
-        numcolumns = data.shape[1]  # how many columns are there in the inut data?
-        numframes = int(max(data[:, numcolumns - 2]) + 1)  # assume framenumber is penultimate column
-        averageparticles = data.shape[0]/float(numframes)
-        sortedmatrix = []
-    
-        for j in xrange(0, numframes):
-            sortedmatrix.append(data[data[:, numcolumns - 2] == j, :])
-    
-        chisquaredresults[i] = cdistmethod(sortedmatrix, numframes, averageparticles, dimensions, numberdensity, threshold)
+    data = np.loadtxt(filename)
 
-    # Print the transpose - this gets the columns and rows right
-    np.savetxt(filename + "_chi4.txt", zip(*chisquaredresults))
-        
-    
-    print time()-t
+    numcolumns = data.shape[1]  # how many columns are there in the inut data?
+    numframes = int(max(data[:, numcolumns - 2]) + 1)  # assume framenumber is penultimate column
+    averageparticles = data.shape[0] / float(numframes)
+    sortedmatrix = []
+
+    for j in range(0, numframes):
+        sortedmatrix.append(data[data[:, numcolumns - 2] == j, :])
+
+    chisquaredresults = cdistmethod(sortedmatrix, numframes, averageparticles, dimensions, numberdensity,
+                                       threshold)
+
+    np.savetxt(filename + "_chi4.txt", chisquaredresults)
+
+    print(time() - t)
+
 
 main()
